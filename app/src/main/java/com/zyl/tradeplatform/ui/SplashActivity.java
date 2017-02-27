c7241dc843b486c79d07700f983038d2ab6c294d
FengChueng <yinglongChueng@gmail.com>
2017-02-27 22:13:38 +0800

    splash activity

--- app/src/main/java/com/zyl/tradeplatform/ui/SplashActivity.java
+++ app/src/main/java/com/zyl/tradeplatform/ui/SplashActivity.java
@@ -1,8 +1,12 @@
 package com.zyl.tradeplatform.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 
+import com.zyl.tradeplatform.MainActivity;
 import com.zyl.tradeplatform.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,5 +15,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },4000);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return true;
     }
 }
